bbd6d80a609ec676615e9fd311ea010e31a8b7b0
rogo6 <piotrrogozinski95@gmail.com>
2018-03-11 19:09:43 +0100

    Created TaxFactory

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
@@ -27,33 +27,9 @@ public class BookKeeper {
 
         for (RequestItem item : items) {
             Money net = item.getTotalCost();
-            BigDecimal ratio = null;
-            String desc = null;
 
-            switch (item.getProductData().getType()) {
-                case DRUG:
-                    ratio = BigDecimal.valueOf(0.02);
-                    desc = "5% (D)";
-					
-                    break;
-                case FOOD:
-                    ratio = BigDecimal.valueOf(0.01);
-                    desc = "7% ";
-                    break;
-                case STANDARD:
-                    ratio = BigDecimal.valueOf(0.33);
-                    desc = "23%";
-                    break;
 
-                default:
-                    throw new IllegalArgumentException(item.getProductData().getType() + " not handled");
-            }
-
-            Money taxValue = net.multiplyBy(ratio);
-
-            Tax tax = new Tax(taxValue, desc);
-
-            InvoiceLine invoiceLine = new InvoiceLine(item.getProductData(), item.getQuantity(), net, tax);
+//            InvoiceLine invoiceLine = new InvoiceLine(item.getProductData(), item.getQuantity(), net, tax);
             invoice.addItem(invoiceLine);
         }
 
